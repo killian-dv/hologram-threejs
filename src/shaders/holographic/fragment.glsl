828f4b2a3ecac3d209e9364a1fd19a82c16b3a01
killian-dv <killian.david@etudiant.univ-reims.fr>
2026-04-06 20:36:16 +0200

    ✨: add normal vector handling and fresnel effect to holographic shader

--- src/shaders/holographic/fragment.glsl
+++ src/shaders/holographic/fragment.glsl
@@ -1,12 +1,26 @@
 varying vec3 vPosition;
+varying vec3 vNormal;
 
 uniform float uTime;
 
 void main() {
+  // normal 
+  vec3 normal = normalize(vNormal);
+
   // stripes
   float stripes = mod((vPosition.y - uTime * 0.02) * 20.0, 1.0);
   stripes = pow(stripes, 3.0);
-  gl_FragColor = vec4(vec3(1.0), stripes);
+
+  // fresnel
+  vec3 viewDirection = normalize(vPosition - cameraPosition);
+  float fresnel = dot(viewDirection, normal) + 1.0;
+  fresnel = pow(fresnel, 2.0);
+
+  // holographic
+  float holographic = fresnel * stripes;
+  holographic += fresnel * 1.25;
+
+  gl_FragColor = vec4(vec3(1.0), holographic);
   #include <tonemapping_fragment>
   #include <colorspace_fragment>
 }
--- src/shaders/holographic/vertex.glsl
+++ src/shaders/holographic/vertex.glsl
@@ -1,4 +1,5 @@
 varying vec3 vPosition;
+varying vec3 vNormal;
 
 void main() {
   vec4 modelPosition = modelMatrix * vec4(position, 1.0);
@@ -6,5 +7,9 @@ void main() {
   // final position
   gl_Position = projectionMatrix * viewMatrix * modelPosition;
 
+  // model normal
+  vec4 modelNormal = modelMatrix * vec4(normal, 0.0);
+
   vPosition = modelPosition.xyz;
+  vNormal = modelNormal.xyz;
 }
