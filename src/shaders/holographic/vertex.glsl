cced9503fe30ffca268c006e63db0c07071600d4
killian-dv <killian.david@etudiant.univ-reims.fr>
2026-04-06 22:29:39 +0200

    ✨: add glitch effect to holographic vertex shader and implement random 2D noise function

--- src/shaders/holographic/vertex.glsl
+++ src/shaders/holographic/vertex.glsl
@@ -1,9 +1,22 @@
 varying vec3 vPosition;
 varying vec3 vNormal;
 
+uniform float uTime;
+
+#include ../includes/random2D.glsl
+
 void main() {
   vec4 modelPosition = modelMatrix * vec4(position, 1.0);
-  
+
+  // glitch
+  float glitchTime = uTime - modelPosition.y;
+  float glitchStrength = sin(glitchTime) + sin(glitchTime * 3.45) + sin(glitchTime * 8.76);
+  glitchStrength /= 3.0;
+  glitchStrength = smoothstep(0.3, 1.0, glitchStrength);
+  glitchStrength *= 0.25;
+  modelPosition.x += (random2D(modelPosition.xz + uTime) - 0.5) * glitchStrength;
+  modelPosition.z += (random2D(modelPosition.zx + uTime) - 0.5) * glitchStrength;
+
   // final position
   gl_Position = projectionMatrix * viewMatrix * modelPosition;
 
